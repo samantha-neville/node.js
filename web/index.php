6d486c0632c37820b349cc06816f76cf6a6857c2
Samantha Neville <samantha.neville99@gmail.com>
2020-01-16 13:11:42 -0700

    file changes

--- web/index.php
+++ web/index.php
@@ -1,4 +1,19 @@
-<?php
-phpinfo();
-echo 'what is going on';
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap Template</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <?php 
+      echo 'im here too';
+    ?>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
