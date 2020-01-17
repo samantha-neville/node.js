56cb237e4706836f957ffc2ceaac1aab1fa3b645
Samantha Neville <samantha.neville99@gmail.com>
2020-01-17 13:06:24 -0700

    about me

--- web/aboutPage/about.php
+++ web/aboutPage/about.php
@@ -1,3 +1,28 @@
- <?php 
-     include 'landingPage/header.php';
-?>
+<link href="landingPage/header.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+<header>
+	<div class="overlay">
+<h1>Samantha Neville - CS 313</h1>
+	<br>
+	<button onclick="window.location.href = 'index.php';">HOME</button> 	
+	<button onclick="window.location.href = 'projects/projects.php';">PROJECTS</button> 	
+    <button onclick="window.location.href = 'aboutPage/about.php';">ABOUT SAMANTHA</button>
+		</div>
+</header>
+
+<section>
+<h3>About Me In 10 Fun Facts</h3>
+<p>
+1. I'm from Missouri. My parents house is actually 20 minutes from the Far West, Missouri Temple site!<br>
+2. My favorite ice cream is chocolate brownie. I love adding mini marshmellows to it. <br>
+3. I love to accompany on the piano. I played the organ in my home ward. <br>
+4. My first ever exposure to programming was CS124. <br>
+5. My husband and I are expecting a little baby boy in June! We haven't decided on a name yet. <br>
+6. I don't love hiking, but I love taking super long walks. (The difference is...elevation!) <br> 
+7. A weird trick I can do is fit my whole fist in my mouth. <br>
+8. Glitter pens are my go to office supply. I use them every. single. day. <br>
+9. My favorite board game is monopoly. No one ever wants to play it though!<br>
+10. I did web dev for my internship, but I still don't really know what I'm doing :) <br>
+
+</p>
+</section>
