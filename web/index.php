9ebe00e00257b61b84c7cba0cb7d51fa8a281e9b
Samantha Neville <samantha.neville99@gmail.com>
2020-01-17 12:52:31 -0700

    first three pages

--- web/index.php
+++ web/index.php
@@ -8,9 +8,8 @@
   </head>
   <body>
     <?php 
-     include 'header.php';
+     include 'landingPage/header.php';
     ?>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   </body>
 </html>
