d7c03bbd82b8754483b886d171e359a4dab6e539
Samantha Neville <samantha.neville99@gmail.com>
2020-01-18 08:32:07 -0700

    stuff

--- web/projects/project3.php
+++ web/projects/project3.php
@@ -4,9 +4,9 @@ include 'https://protected-refuge-92528.herokuapp.com/landingPage/header.php';
 include 'landingPage/header.php';
 include 'header.php';
 
+?>
+
 <button onclick="window.location.href = 'https://protected-refuge-92528.herokuapp.com/projects/project1.php';">Project 1</button> 	
 <button onclick="window.location.href = 'https://protected-refuge-92528.herokuapp.com/projects/project2.php';">Project 2</button> 	
 <button onclick="window.location.href = 'https://protected-refuge-92528.herokuapp.com/projects/project3.php';">Project 3</button>
 <button onclick="window.location.href = 'https://protected-refuge-92528.herokuapp.com/retreats/index.php';">Creative Heartbeats</button>
-
-?>
