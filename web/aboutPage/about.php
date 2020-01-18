423207ff81983bc59b85938c687d0a868e2923ff
Samantha Neville <samantha.neville99@gmail.com>
2020-01-17 21:20:17 -0700

    path fixes

--- web/aboutPage/about.php
+++ web/aboutPage/about.php
@@ -1,7 +1,8 @@
 <?php
 
 include 'https://protected-refuge-92528.herokuapp.com/landingPage/header.php';
-
+include 'landingPage/header.php';
+include 'header.php';
 ?>
 
 <section>
--- web/projects/projects.php
+++ web/projects/projects.php
@@ -1,5 +1,6 @@
 <?php
 
 include 'https://protected-refuge-92528.herokuapp.com/landingPage/header.php';
-
+include 'landingPage/header.php';
+include 'header.php';
 ?>
