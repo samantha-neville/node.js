e9793deccd22bd231b05e9253f50d6d065e26e7a
Samantha Neville <samantha.neville99@gmail.com>
2020-01-21 16:27:11 -0700

    store

--- web/classActivity.php
+++ web/classActivity.php
@@ -1,8 +1,23 @@
 <?php
 //get post data
-$email = htmlspecialchars($_POST['email']);
+$email    = htmlspecialchars($_POST['email']);
 $password = htmlspecialchars($_POST['password']);
+$major    = htmlspecialchars($_POST['major']);
+$name     = htmlspecialchars($_POST['name']);
+$comments = htmlspecialchars($_POST['comments']);
+$continent = isset($_POST['continent']) ? htmlspecialchars($_POST['continent']) : array();
 
-//do stuff with the data
-echo "Email: $email<br> Password: $password";
+//display the data data
+echo "Name: $name <br>";
+echo "<a href='mailto:$email'>$email</a><br>"; //making this a link
+echo "Major: $major <br>";
+echo "Comments: $comments <br>";
+if ($continent) {
+    echo "You have selected the continents: <br>"; 
+    foreach($continent as $value) {
+        echo " " . $value . "<br>";
+    }
+}
+else 
+    echo "You haven't selected any continents.";
 ?>
