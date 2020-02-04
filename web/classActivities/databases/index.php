9092f4c214aa2b99a03ad195e51c1f05b5a06351
Samantha Neville <samantha.neville99@gmail.com>
2020-02-04 16:27:21 -0700

    class activity

--- web/classActivities/databases/index.php
+++ web/classActivities/databases/index.php
@@ -1,32 +1,32 @@
 <?php
     require 'dbConnection.php';
-    $database = getDB();
-    $familyMembers = $database->prepare("SELECT * FROM w5_family_members");
-    $familyMembers->execute();
+    $db = getDB();
+    // $familyMembers = $db->prepare("SELECT * FROM w5_family_members");
+    // $familyMembers->execute();
 
-    while($row = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
-        $fname = $row['first_name'];
-        $lname = $row['last_name'];
-        $relationshipID = $row['relationship_id'];
+    // while($row = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
+    //     $fname = $row['first_name'];
+    //     $lname = $row['last_name'];
+    //     $relationshipID = $row['relationship_id'];
 
-        $relationship = $database->prepare("SELECT description FROM w5_relationships WHERE id=$relationshipID");
-        $relationship->execute();
-        while($row2 = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
-            $relationship = $row2['description'];
-        }
+    //     $relationship = $db->prepare("SELECT description FROM w5_relationships WHERE id=$relationshipID");
+    //     $relationship->execute();
+    //     while($row2 = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
+    //         $relationship = $row2['description'];
+    //     }
 
-        echo "<p>$fname $lname is my $relationship ($relationshipID)</p>";
-    }
+    //     echo "<p>$fname $lname is my $relationship ($relationshipID)</p>";
+    // }
 
-    $scriptures = $database->prepare("SELECT * FROM scriptures");
+    $scriptures = $db->prepare("SELECT * FROM scriptures");
     $scriptures->execute();
 
     echo "<br><br><h1>Scripture Resources</h1>";
     while($sRow  = $scriptures->fetch(PDO::FETCH_ASSOC)) {
-        $book    = $row['book'];
-        $chapter = $row['chapter'];
-        $verse   = $row['verse'];
-        $content = $row['content'];
+        $book    = $sRow['book'];
+        $chapter = $sRow['chapter'];
+        $verse   = $sRow['verse'];
+        $content = $sRow['content'];
 
         echo "<p><b>$book $chapter:$verse</b> - \"$content\"</p><br>";
     }
