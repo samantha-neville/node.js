9383083aba73ca135638f78c312638e0e87abaed
Samantha Neville <samantha.neville99@gmail.com>
2020-02-04 16:30:08 -0700

    team activity

--- web/classActivities/databases/index.php
+++ web/classActivities/databases/index.php
@@ -1,22 +1,22 @@
 <?php
     require 'dbConnection.php';
     $db = getDB();
-    // $familyMembers = $db->prepare("SELECT * FROM w5_family_members");
-    // $familyMembers->execute();
+    $familyMembers = $db->prepare("SELECT * FROM w5_family_members");
+    $familyMembers->execute();
 
-    // while($row = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
-    //     $fname = $row['first_name'];
-    //     $lname = $row['last_name'];
-    //     $relationshipID = $row['relationship_id'];
+    while($row = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
+        $fname = $row['first_name'];
+        $lname = $row['last_name'];
+        $relationshipID = $row['relationship_id'];
 
-    //     $relationship = $db->prepare("SELECT description FROM w5_relationships WHERE id=$relationshipID");
-    //     $relationship->execute();
-    //     while($row2 = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
-    //         $relationship = $row2['description'];
-    //     }
+        $relationship = $db->prepare("SELECT description FROM w5_relationships WHERE id=$relationshipID");
+        $relationship->execute();
+        while($row2 = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
+            $relationship = $row2['description'];
+        }
 
-    //     echo "<p>$fname $lname is my $relationship ($relationshipID)</p>";
-    // }
+        echo "<p>$fname $lname is my $relationship ($relationshipID)</p>";
+    }
 
     $scriptures = $db->prepare("SELECT * FROM scriptures");
     $scriptures->execute();
