ed01a9a0367fdb3fad09c76d3a493f8f5c16dc7c
Samantha Neville <samantha.neville99@gmail.com>
2020-02-04 16:45:14 -0700

    team activity

---
+++ web/classActivities/databases/displayScriptures.php
@@ -0,0 +1,19 @@
+    
+    <?php
+    require 'dbConnection.php';
+    $db = getDB();
+    
+    $book = $_POST['book'];
+    $scriptures = $db->prepare("SELECT * FROM scriptures WHERE book=$book");
+    $scriptures->execute();
+
+    echo "<br><br><h1>Scripture Resources</h1>";
+    while($sRow  = $scriptures->fetch(PDO::FETCH_ASSOC)) {
+        $book    = $sRow['book'];
+        $chapter = $sRow['chapter'];
+        $verse   = $sRow['verse'];
+        $content = $sRow['content'];
+
+        echo "<p><b>$book $chapter:$verse</b> - \"$content\"</p><br>";
+    }
+    ?>
--- web/classActivities/databases/index.php
+++ web/classActivities/databases/index.php
@@ -4,7 +4,7 @@
 
 <h2>HTML Forms</h2>
 
-<form action="" method='POST'>
+<form action="displayScriptures.php" method='POST'>
   Book:<br>
   <input type="text" name="book">
   <br>
@@ -15,39 +15,23 @@
 </html>
 
 <?php
-    require 'dbConnection.php';
-    $db = getDB();
-    $familyMembers = $db->prepare("SELECT * FROM w5_family_members");
-    $familyMembers->execute();
 
-    while($row = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
-        $fname = $row['first_name'];
-        $lname = $row['last_name'];
-        $relationshipID = $row['relationship_id'];
+    // $familyMembers = $db->prepare("SELECT * FROM w5_family_members");
+    // $familyMembers->execute();
 
-        $relationship = $db->prepare("SELECT description FROM w5_relationships WHERE id=$relationshipID");
-        $relationship->execute();
-        while($row2 = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
-            $relationship = $row2['description'];
-        }
+    // while($row = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
+    //     $fname = $row['first_name'];
+    //     $lname = $row['last_name'];
+    //     $relationshipID = $row['relationship_id'];
 
-        echo "<p>$fname $lname is my $relationship ($relationshipID)</p>";
-    }
+    //     $relationship = $db->prepare("SELECT description FROM w5_relationships WHERE id=$relationshipID");
+    //     $relationship->execute();
+    //     while($row2 = $familyMembers->fetch(PDO::FETCH_ASSOC)) {
+    //         $relationship = $row2['description'];
+    //     }
 
-
-    $book = $_POST['book'];
-    $scriptures = $db->prepare("SELECT * FROM scriptures WHERE book=$book");
-    $scriptures->execute();
-
-    echo "<br><br><h1>Scripture Resources</h1>";
-    while($sRow  = $scriptures->fetch(PDO::FETCH_ASSOC)) {
-        $book    = $sRow['book'];
-        $chapter = $sRow['chapter'];
-        $verse   = $sRow['verse'];
-        $content = $sRow['content'];
-
-        echo "<p><b>$book $chapter:$verse</b> - \"$content\"</p><br>";
-    }
+    //     echo "<p>$fname $lname is my $relationship ($relationshipID)</p>";
+    // }
 
 
 ?>
