40bf3dae31c202de8cd8a2a01c682b178781e62a
Samantha Neville <samantha.neville99@gmail.com>
2020-02-06 17:58:52 -0700

    retreats project

--- web/projects/creativeHeartbeats/theme/index.php
+++ web/projects/creativeHeartbeats/theme/index.php
@@ -47,7 +47,7 @@
               <div class="col-lg-12 col-md-9">
                 <!-- <input type="email" class="form-control form-control-lg" placeholder="Enter your email..."> -->
                 <?php
-                include $_SERVER["DOCUMENT_ROOT"].'/projects/creativeHeartbeats/theme/navbar/index.php';
+                require $_SERVER["DOCUMENT_ROOT"].'/projects/creativeHeartbeats/theme/navbar/index.php';
               ?>
               </div>
               <!-- <div class="col-12 col-md-3">
