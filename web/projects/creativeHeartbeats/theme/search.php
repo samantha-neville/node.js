fdcd6785cb824a6096af1331b2766a1af5d0b35a
Samantha Neville <samantha.neville99@gmail.com>
2020-02-06 18:28:05 -0700

    retreats project

--- web/projects/creativeHeartbeats/theme/search.php
+++ web/projects/creativeHeartbeats/theme/search.php
@@ -1,7 +1,20 @@
     
     <?php
-    // require 'dbConnection.php';
-    // $db = getDB();
+    require 'dbConnection.php';
+    $db = getDB();
+    // $book = $_POST['book'];
+    // $scriptures = $db->prepare("SELECT * FROM scriptures WHERE book="$book");
+    // $scriptures->execute();
+
+    // echo "<br><br><h1>Scripture Resources</h1>";
+    // while($sRow  = $scriptures->fetch(PDO::FETCH_ASSOC)) {
+    //     $book    = $sRow['book'];
+    //     $chapter = $sRow['chapter'];
+    //     $verse   = $sRow['verse'];
+    //     $content = $sRow['content'];
+
+    //     echo "<p><b>$book $chapter:$verse</b> - \"$content\"</p><br>";
+    // }
     ?>
     <!DOCTYPE html>
 <html>
