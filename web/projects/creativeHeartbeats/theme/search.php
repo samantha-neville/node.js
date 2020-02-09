aa659214f5ded6a0e437d6f0c33f45fbb992cdee
Samantha Neville <samantha.neville99@gmail.com>
2020-02-08 21:48:34 -0700

    retreat stuff

--- web/projects/creativeHeartbeats/theme/search.php
+++ web/projects/creativeHeartbeats/theme/search.php
@@ -27,6 +27,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom fonts for this template -->
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+<!-- Custom styles for this template -->
+<link href="css/landing-page.min.css" rel="stylesheet">
 <style>
 /* flexbox stuff is from w3 schools */
 .flex-container {
@@ -57,16 +66,10 @@ p {
 .column {
   max-width:50%;
 }
+.retreat-title {
+  font-size:18px;
+}
 </style>
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Custom fonts for this template -->
-<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-<link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-<!-- Custom styles for this template -->
-<link href="css/landing-page.min.css" rel="stylesheet">
 </head>
 <body>
 <?php
@@ -95,7 +98,7 @@ require 'navbar.php';
     echo "
     <div>
       <div class='row'>
-      <p><b>$name</b><br> $desc</p>
+      <p><b>$name</b><br><br> $desc</p><br><br>
         <div class='column'>
           <br><b><p class='left-align'>Location:</b> $location</p>
           <b><p class='left-align'>Price:</b> $$price</p>
