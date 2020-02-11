7b97905bfd8bbb30d0437eb31034596da90594d0
Samantha Neville <samantha.neville99@gmail.com>
2020-02-11 16:41:17 -0700

    class activity

--- web/classActivities/databases/saveTopic.php
+++ web/classActivities/databases/saveTopic.php
@@ -2,6 +2,7 @@
   require 'dbConnection.php';
   $db = getDB();
  //query1
+ try {
   $query  = "INSERT INTO scriptures (book, chapter, verse, content) VALUES ($_POST['book'], $_POST['chapter'], $_POST['verse'], $_POST['content'])";
   echo $query;
   $statement = $db->prepare($query);
@@ -12,5 +13,12 @@
   echo $query2;
   $statement2 = $db->prepare($query2);
   $statement2->execute();
+ }
+ catch(Exception $e) {
+     echo "error with Db $e";
+     die();
+ }
+header("saveTopic.php/?scriptureID=$scriptureID");
+ die();
 
 ?>
