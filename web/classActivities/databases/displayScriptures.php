748f85840a82d3c59f9451826265d4162818e51e
Samantha Neville <samantha.neville99@gmail.com>
2020-02-11 16:03:54 -0700

    class activity

--- web/classActivities/databases/displayScriptures.php
+++ web/classActivities/databases/displayScriptures.php
@@ -10,12 +10,12 @@
     $scriptures->execute();
 
     echo "<br><br><h1>Scripture Resources</h1>";
-    // while($sRow  = $scriptures->fetch(PDO::FETCH_ASSOC)) {
-    //     $book    = $sRow['book'];
-    //     $chapter = $sRow['chapter'];
-    //     $verse   = $sRow['verse'];
-    //     $content = $sRow['content'];
+    while($sRow  = $scriptures->fetch(PDO::FETCH_ASSOC)) {
+        $book    = $sRow['book'];
+        $chapter = $sRow['chapter'];
+        $verse   = $sRow['verse'];
+        $content = $sRow['content'];
 
-    //     echo "<p><b>$book $chapter:$verse</b> - \"$content\"</p><br>";
-    // }
+        // echo "<p><b>$book $chapter:$verse</b> - \"$content\"</p><br>";
+    }
     ?>
