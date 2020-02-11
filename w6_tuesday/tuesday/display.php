17fca3ffc85a3b5542f66d92816a6a252089573a
Samantha Neville <samantha.neville99@gmail.com>
2020-02-11 15:44:12 -0700

    Class activity

--- w6_tuesday/tuesday/display.php
+++ w6_tuesday/tuesday/display.php
@@ -5,6 +5,23 @@
 	<body>
 		<div class="container">
          <?php
+         $personId - $_GET['personId'];
+         $statment = $db->prepare('SELECT * FROM w6_user WHERE ID = :personId');
+         $statment->bindValue(':personId', $personId);
+         $statment->execute();
+         while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $id      = $row['id'];
+            $first   = $row['first_name'];
+            $last    = $row['last_name'];
+            $food_id = $row['food_type'];
+            $personId - $_GET['personId'];
+            $statment = $db->prepare('SELECT food FROM w6_food WHERE ID = $food_id');
+            $statment->execute();
+            while($row2 = $statement->fetch(PDO::FETCH_ASSOC)) {
+               $food = $row2['food'];
+            }
+            echo "<h1>$first $last's favorite food is $food</h1>";
+         }
             // retrieve url parameter
             // execute query to pull up data from that id
             // execute another query to get food data
