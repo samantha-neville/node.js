0733e2be5f80ee1a0d37a9c11e9a0107f0ae9156
Samantha Neville <samantha.neville99@gmail.com>
2020-02-15 15:49:39 -0700

    retreat project

--- web/projects/creativeHeartbeats/theme/insertRetreat.php
+++ web/projects/creativeHeartbeats/theme/insertRetreat.php
@@ -27,7 +27,7 @@
     require 'dbConnection.php';
     $db = getDB();
 
-    $query = "INSERT INTO retreats(price, location, type, description, start_date, end_date, duration, group_size, language, cancel_policy, host_id, name) VALUES ($price, '$location', '$type', '$desc, '$sDate', '$eDate', $duration, $size, '$lang', '$cancel', 1, '$name');";
+    $query = "INSERT INTO retreats(price, location, type, description, start_date, end_date, duration, group_size, language, cancel_policy, host_id, name) VALUES ($price, '$location', '$type', '$desc', '$sDate', '$eDate', $duration, $size, '$lang', '$cancel', 1, '$name');";
     echo $query;
     try {
         $retreat = $db->prepare($query);
