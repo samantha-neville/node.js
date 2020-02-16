a61198642356bbc1504e6d3d39ef7bfa7dca39bc
Samantha Neville <samantha.neville99@gmail.com>
2020-02-15 22:12:56 -0700

    retreat project

--- web/projects/creativeHeartbeats/theme/insertRetreat.php
+++ web/projects/creativeHeartbeats/theme/insertRetreat.php
@@ -24,38 +24,32 @@
         $user = $db->prepare($query1);
         $user->execute();
         if ($user->rowCount() > 0) {
-            echo 'match<br>';
             //they are already in the database. get their user id, and use it find their current host id
             while($row = $user->fetch(PDO::FETCH_ASSOC)) {
-                echo "in the while loop<br>";
                 $userId = $row['id'];
-                echo "userid $userId<br>";
                 //query 2: use their user id to find their host id
-                $query3 = "SELECT * FROM hosts WHERE user_id=$userId";
-                echo "query3 $query3<br>";
-                $host = $db->prepare($query3);
+                $query2 = "SELECT * FROM hosts WHERE user_id=$userId";
+                $host = $db->prepare($query2);
                 $host->execute();
                 while($row2 = $host->fetch(PDO::FETCH_ASSOC)) {
                     $hostId = $row2['id'];
-                    echo $hostId;
                 }
 
             }
         }
         else {
-            echo"in the else<Br>";
             //they are not in the database. we will have to add them as a user and a host to get their host id
             //query 2: add the person to the database as a user
             $query2 = "INSERT INTO users (name, email, password, last_name) VALUES ('$first', '$email', 'pass123', '$last')";
-            // $user = $db->prepare($query2);
-            // $user->execute();
-            // $userId = $db->lastInsertId("users_id_seq");
+            $user = $db->prepare($query2);
+            $user->execute();
+            $userId = $db->lastInsertId("users_id_seq");
 
             //query 3: now that we have the host as a user, insert them into the hosts table with their new user id and host desc.
             $query3 = "INSERT INTO hosts (user_id, about_host) VALUES ($userId, '$host')";
-            // $host = $db->prepare($query3);
-            // $host->execute();
-            // $hostId = $db->lastInsertId("hosts_id_seq");
+            $host = $db->prepare($query3);
+            $host->execute();
+            $hostId = $db->lastInsertId("hosts_id_seq");
 
         }
 
@@ -67,15 +61,14 @@
 
 
     $query = "INSERT INTO retreats(price, location, type, description, start_date, end_date, duration, group_size, language, cancel_policy, host_id, name) VALUES ($price, '$location', '$type', '$desc', '$sDate', '$eDate', $duration, $size, '$lang', '$cancel', $hostId, '$name');";
-    echo $query;
-    // try {
-    //     $retreat = $db->prepare($query);
-    //     $retreat->execute();
-    // }
-    // catch(Exception $e) {
-    //     echo "Database Error. $e";
-    //     die();
-    // }
+    try {
+        $retreat = $db->prepare($query);
+        $retreat->execute();
+    }
+    catch(Exception $e) {
+        echo "Database Error. $e";
+        die();
+    }
     ?>
 
 <!DOCTYPE html>
