00f5ce2f69f044e8f5ec01f5db45038024301654
Samantha Neville <samantha.neville99@gmail.com>
2020-02-20 16:46:47 -0700

    team activity 07

--- web/projects/creativeHeartbeats/theme/validateSignIn.php
+++ web/projects/creativeHeartbeats/theme/validateSignIn.php
@@ -6,25 +6,25 @@
     //get the email and password from their login attempt
     $email    = htmlspecialchars($_POST['email']);
     $password = htmlspecialchars($_POST['password']);
-    echo "password:<br>$password";
+    echo "password:$password";
     echo "<br>email:$email";
    
    //look to see if this email is indeed in our database (!!NEED TO MAKE EMAIL UNIQUE IN DATABASE!!)
    try {
         $query = "SELECT id FROM users WHERE email='$email'";
-        echo "query:<br>$query<br>";
-        echo "password:<br>$password, hashed password: $hashed_password<br>";
-        echo "<br>email:$email";
+        echo "<br>query:$query<br>";
         $user = $db->prepare($query);
         $user->execute();
         if ($user->rowCount() > 0) {
             //they are already in the database. get their user id, and use it find their current host id
             while($row = $user->fetch(PDO::FETCH_ASSOC)) {
                 $hashed_password = $row['password'];
+                echo "<br>hashedPassword:$hashed_password";
                 if(password_verify($password, $hashed_password)) {
                     echo "<br>we have a matched email/password combo";
                     session_start();
                     $_SESSION['userId'] =  $row['id'];
+                    echo "<br>session id:" . $_SESSION['userId'];
                 }
             }
             //    header('Location: signIn.php');
