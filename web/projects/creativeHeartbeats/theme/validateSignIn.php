405a4a9b4badd2219ac76e39b5de070b98edd0be
Samantha Neville <samantha.neville99@gmail.com>
2020-02-20 16:37:57 -0700

    team activity 07

--- web/projects/creativeHeartbeats/theme/validateSignIn.php
+++ web/projects/creativeHeartbeats/theme/validateSignIn.php
@@ -11,30 +11,31 @@
     echo "password:<br>$password, hashed password: $hashed_password<br>";
     echo "<br>email:$email";
    
-//    //look to see if this email is indeed in our database (!!NEED TO MAKE EMAIL UNIQUE IN DATABASE!!)
-//    try {
-//     $query = "SELECT id FROM users WHERE email='$email'";
-//     echo "query:<br>$query<br>";
-//     echo "password:<br>$password, hashed password: $hashed_password<br>";
-//     echo "<br>email:$email";
-//     $user = $db->prepare($query);
-//     $user->execute();
-//     if ($user->rowCount() > 0) {
-//         //they are already in the database. get their user id, and use it find their current host id
-//         while($row = $user->fetch(PDO::FETCH_ASSOC)) {
-//             if(password_verify($password, $hashed_password)) {
-//                 echo "<br>we have a matched email/password combo";
-//                 // session_start();
-//                 // $_SESSION['userId'] =  ;
-//             }
-//         }
-//     //    header('Location: signIn.php');
-//        die();
-//    }
-//    catch(Exception $e) {
-//        echo "Database Error. $e";
-//        die();
-//    }
+   //look to see if this email is indeed in our database (!!NEED TO MAKE EMAIL UNIQUE IN DATABASE!!)
+   try {
+        $query = "SELECT id FROM users WHERE email='$email'";
+        echo "query:<br>$query<br>";
+        echo "password:<br>$password, hashed password: $hashed_password<br>";
+        echo "<br>email:$email";
+        $user = $db->prepare($query);
+        $user->execute();
+        if ($user->rowCount() > 0) {
+            //they are already in the database. get their user id, and use it find their current host id
+            while($row = $user->fetch(PDO::FETCH_ASSOC)) {
+                if(password_verify($password, $hashed_password)) {
+                    echo "<br>we have a matched email/password combo";
+                    // session_start();
+                    // $_SESSION['userId'] =  ;
+                }
+            }
+            //    header('Location: signIn.php');
+            die();
+        }
+    }
+   catch(Exception $e) {
+       echo "Database Error. $e";
+       die();
+   }
 
 
  
