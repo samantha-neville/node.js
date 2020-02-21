7b420e0c52caa9e06e7097581194c3d21abfb0b7
Samantha Neville <samantha.neville99@gmail.com>
2020-02-21 15:05:21 -0700

    team activity 07

--- web/projects/creativeHeartbeats/theme/validateSignIn.php
+++ web/projects/creativeHeartbeats/theme/validateSignIn.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+</style>
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom fonts for this template -->
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+<!-- Custom styles for this template -->
+<link href="css/landing-page.min.css" rel="stylesheet">
+<!-- <link href="nav.css" rel="stylesheet" type="text/css"> -->
+</head>
+<body>
+<?php
+    require 'navbar.php';
+?>
+<br><br><br>
+<div class="container2">
+
+<h1>Sign In</h1>
+<form action="validateSignIn.php" method="POST">
+    <!--Email -->
+    <label for="email" required>Email</label>
+    <input type='email' class='stuff' type="text" id="email" name="email" placeholder="example@example.com" required>
+    <!-- Password -->
+    <label for="password">Password</label>
+    <input class='stuff' type="password" id="password" name="password" placeholder="password">
+    <input type="submit" class='submit'  value="Log In">
+  </form>
+</div>
+</body>
+</html> 
+ 
+
 <?php
     //database time
     require 'dbConnection.php';
@@ -31,7 +70,7 @@
                 else {
                     //take them back to the signIn.php. password doesn't match
                     echo "Wrong Password";
-                    header('Refresh: 5; URL=signIn.php');   
+                    header('Refresh: 3; URL=signIn.php');   
                     die();
                 }
             }
@@ -39,7 +78,7 @@
         else {
                 //take them back to the signIn.php. no matching email
                 echo "No matching email";
-                header('Refresh: ; URL=signIn.php');   
+                header('Refresh: 3; URL=signIn.php');   
                 die();
         }
     }
