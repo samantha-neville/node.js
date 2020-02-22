76f8a0dfb0f4bec8d1e341973b688c82cf54fe2d
Samantha Neville <samantha.neville99@gmail.com>
2020-02-21 18:31:14 -0700

    retreat

--- web/projects/creativeHeartbeats/theme/attend.php
+++ web/projects/creativeHeartbeats/theme/attend.php
@@ -1,51 +1,64 @@
 <!DOCTYPE html>
-<html>
-<head>
-<style>
+<html lang="en">
 
-</style>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Creative Heartbeats Retreats | Attend</title>
+
+  <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-<!-- Custom fonts for this template -->
-<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-<link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
-<!-- Custom styles for this template -->
-<link href="css/landing-page.min.css" rel="stylesheet">
-<!-- <link href="nav.css" rel="stylesheet" type="text/css"> -->
+  <!-- Custom styles for this template -->
+  <link href="css/landing-page.css" rel="stylesheet">
+  <!-- <link href="nav.css" rel="stylesheet" type="text/css"> -->
 </head>
+
 <body>
 <?php
-    require 'navbar.php';
+require 'navbar.php';
 ?>
-<br><br><br>
-<h1>Contact Us</h1>
 
-<p>Questions, thoughts or comments? Contact us!</p>
+  <!-- Masthead -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <!-- <div class="col-xl-12"> -->
+          <!-- <h1 class="mb-5">Find your retreat</h1> -->
+        <!-- </div> -->
+        <div class="col-md-12 col-lg-8 col-xl-7 mx-auto">
+          <!-- <form> -->
+            <div class="form-row">
+              <div class="col-lg-12 col-md-9">
+                <!-- <input type="email" class="form-control form-control-lg" placeholder="Enter your email..."> -->
+                <?php
+                require $_SERVER["DOCUMENT_ROOT"].'/projects/creativeHeartbeats/theme/searchbar/index.php';
+              ?>
+              </div>
+              <!-- <div class="col-12 col-md-3">
+                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
+              </div> -->
+            </div>
+          <!-- </form> -->
+        </div>
+      </div>
+    </div>
+  </header>
 
-<div class="container2">
-  <form action="/action_page.php">
-    <label for="fname">First Name</label>
-    <input class='stuff' type="text" id="fname" name="firstname" placeholder="Your name..">
-
-    <label for="lname">Last Name</label>
-    <input class='stuff' type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-    <label for="country">Country</label>
-    <select class='stuff' id="country" name="country">
-      <option value="australia">Australia</option>
-      <option value="canada">Canada</option>
-      <option value="usa">USA</option>
-    </select>
-
-    <label for="subject">Subject</label>
-    <textarea class='stuff' id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
-
-    <input type="submit" class='submit'  value="Submit">
-  </form>
-</div>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
-</html> 
- 
+
+</html>
