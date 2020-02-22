37a427f13b2b7de810a4eed129886a0c91b16393
Samantha Neville <samantha.neville99@gmail.com>
2020-02-22 11:48:08 -0700

    retreat

--- web/projects/creativeHeartbeats/theme/displayAll.php
+++ web/projects/creativeHeartbeats/theme/displayAll.php
@@ -74,6 +74,7 @@ require 'navbar.php';
 <!-- <p>Click on a retreat to learn more!</p> -->
 
 <?php
+  $count = 0;
   while($rRow  = $retreats->fetch(PDO::FETCH_ASSOC)) {
     $name     = $rRow['name'];
     $desc     = $rRow['description'];
@@ -88,29 +89,49 @@ require 'navbar.php';
     $eDate    = $rRow['end_date'];
     $host     = $rRow['host_id'];
 
+  
 
     $query2 = "SELECT about_host FROM hosts WHERE id=$host";
     $statement = $db->prepare($query2);
     $statement->execute();
     while($hRow  = $statement->fetch(PDO::FETCH_ASSOC)) {
       $host = $hRow['about_host'];
+
+    }
+
+    if ($count % 2 == 0) {
+        echo "
+        <div class='flex-container'>
+        <div>
+          <p><b class='retreat-title'>$name</b><br><br> $desc</p><br><br>
+              <br><b><p class='left-align'>Location:</b> $location</p>
+              <b><p class='left-align'>Price:</b> $$price</p>
+              <b><p class='left-align'>Language:</b> $lang</p>
+              <b><p class='left-align'>Group Size:</b> $size people</p>
+              <b><p class='left-align'>Duration:</b> $duration days</p>
+              <b><p class='left-align'>Dates:</b> $sDate - $eDate</p>   
+              <b><p class='left-align'>About the host:</b> $host</p>  
+              <p class='left-align margin-40'><a class='search-btn no-margin' href='displayAll.php'>Sign Up</a></p>
+        </div>
+        </div>";
+    }
+    else {
+        echo "
+        <div>
+          <p><b class='retreat-title'>$name</b><br><br> $desc</p><br><br>
+              <br><b><p class='left-align'>Location:</b> $location</p>
+              <b><p class='left-align'>Price:</b> $$price</p>
+              <b><p class='left-align'>Language:</b> $lang</p>
+              <b><p class='left-align'>Group Size:</b> $size people</p>
+              <b><p class='left-align'>Duration:</b> $duration days</p>
+              <b><p class='left-align'>Dates:</b> $sDate - $eDate</p>   
+              <b><p class='left-align'>About the host:</b> $host</p>  
+              <p class='left-align margin-40'><a class='search-btn no-margin' href='displayAll.php'>Sign Up</a></p>
+        </div>";
     }
 
 
-    echo "
-    <div class='flex-container'>
-    <div>
-      <p><b class='retreat-title'>$name</b><br><br> $desc</p><br><br>
-          <br><b><p class='left-align'>Location:</b> $location</p>
-          <b><p class='left-align'>Price:</b> $$price</p>
-          <b><p class='left-align'>Language:</b> $lang</p>
-          <b><p class='left-align'>Group Size:</b> $size people</p>
-          <b><p class='left-align'>Duration:</b> $duration days</p>
-          <b><p class='left-align'>Dates:</b> $sDate - $eDate</p>   
-          <b><p class='left-align'>About the host:</b> $host</p>  
-          <p class='left-align margin-40'><a class='search-btn no-margin' href='displayAll.php'>Sign Up</a></p>
-    </div>
-    </div>";
+    $count = $count + 1;
 }
 ?>
 
