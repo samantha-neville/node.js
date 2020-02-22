0d60bbe55d57bc07421348448fe3f15d1be128ff
Samantha Neville <samantha.neville99@gmail.com>
2020-02-22 11:40:03 -0700

    retreat

--- web/projects/creativeHeartbeats/theme/displayAll.php
+++ web/projects/creativeHeartbeats/theme/displayAll.php
@@ -32,13 +32,19 @@
 
 .flex-container > div {
     background-color: #f1f1f1;
-    width: 40%;
+    /* width: 80%;
     margin: 10px;
     text-align: center;
     text-align: center;
     margin:10px 10% 10px 10%;
     padding:20px;
-    border-radius:10px;
+    border-radius:10px; */
+    background-color: #f1f1f1;
+  width: 100px;
+  margin: 10px;
+  text-align: center;
+  line-height: 75px;
+  font-size: 30px;
 }
 
 h1 {
