09a3d92f61ba7ddcf8ed56bef39ffc8b8019ea7f
Samantha Neville <samantha.neville99@gmail.com>
2020-02-22 14:57:37 -0700

    retreat

--- web/projects/creativeHeartbeats/theme/retreatSignUp.php
+++ web/projects/creativeHeartbeats/theme/retreatSignUp.php
@@ -1,3 +1,13 @@
+<?php
+    $name = htmlspecialchars($_POST['name']);
+    $location = htmlspecialchars($_POST['location']);
+    $price = htmlspecialchars($_POST['price']);
+    $size = htmlspecialchars($_POST['size']);
+    $language = htmlspecialchars($_POST['language']);
+    echo "$name $location $price $size $language";
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
