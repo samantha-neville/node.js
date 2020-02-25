21afb59ff287e8046fe68aff6dbb81d59fa9f4d7
Samantha Neville <samantha.neville99@gmail.com>
2020-02-25 16:20:21 -0700

    retreat

--- web/projects/creativeHeartbeats/theme/displayAll.php
+++ web/projects/creativeHeartbeats/theme/displayAll.php
@@ -111,7 +111,7 @@ require 'navbar.php';
           <b><p class='left-align'>Duration:</b> $duration days</p>
           <b><p class='left-align'>Dates:</b> $sDate - $eDate</p>   
           <b><p class='left-align'>About the host:</b> $host</p>
-          <p class='left-align margin-40'><button class='search-btn no-margin' type='submit'>Sign Up</button></p>       
+          <p class='left-align margin-40'><button class='search-btn2' type='submit'>Sign Up</button></p>       
     </form>
     </div>";
 
