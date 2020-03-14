b73252e862362744753731ae283b2a3065833614
Samantha Neville <samantha.neville99@gmail.com>
2020-03-14 12:16:25 -0600

    retreat node

--- web/nodeRetreat/index.js
+++ web/nodeRetreat/index.js
@@ -1,4 +1,4 @@
-//set up our server
+//set up our server and all that jazzy stuff
 const express = require('express')
 const { Pool } = require('pg')
 const app = express()
@@ -13,69 +13,92 @@ module.exports = router;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 //decide where static files come from
-app.use(express.static('public'))
+app.use(express.static('public'))  
+//decide where our views (ejs stuff) comes from
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-
-var sql = "SELECT * FROM users";
-//a simple get query from the database
-pool.query(sql, function(err, result) {
-    // If an error occurred...
-    if (err) {
-        console.log("Error in query: ")
-        console.log(err);
-    }
-
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
-    console.log(result.rows);
-
-
-});   
-
-
+/****************************************************************
+ * HOME - where the app will go when a form requests /search
+ ****************************************************************/
 app.get('/home', function(req, res) {
     res.send("you are home");
 
 });
+/****************************************************************
+ * DISPLAY ALL - where the app will go when a form requests /search
+ ****************************************************************/
 app.get('/displayAll', function(req, res) {
-    res.send("display all yep");
-
-});
-app.get('/search', function(req, res) {
-    res.send("searchy searchy  loady");
-
-});
-
-
-//making an endpoint to get all of the people
-app.get('/getPerson', function(req, res, next) {
-	const id = req.query.id;
-    var sql = "SELECT * FROM person WHERE id = $1::int";
-    console.log('get person from DB with ID' + id);
-
-    const params = [id];
-
+    
+    var sql = "SELECT * FROM retreats";
     //a simple get query from the database
-    pool.query(sql, params, function(err, result) {
-		// If an error occurred...
-		if (err) {
-			console.log("Error in query: ")
-			console.log(err);
-			callback(err, null);
-		}
+    pool.query(sql, function(err, result) {
+        // If an error occurred...
+        if (err) {
+            console.log("Error in query: ")
+            console.log(err);
+        }
 
-		// Log this to the console for debugging purposes.
-		// console.log("Found result: " + JSON.stringify(result.rows));
+        // Log this to the console for debugging purposes.
+        console.log("Back from DB with result:");
+        console.log(result.rows);
+        var jsonRetreats = JSON.stringify(result.rows);
+        // Set up a JSON object of the values we want to pass along to the EJS result page
+        const params = {jsonRetreats: jsonRetreats};
+        displayRetreats(params, res);
 
 
-		// When someone else called this function, they supplied the function
-		// they wanted called when we were all done. Call that function now
-		// and pass it the results.
+    }); 
+    // res.send("display all yep");
 
-		// (The first parameter is the error variable, so we will pass null.)
-		// callback(null, result.rows);
-	});
-  });
 
+});
+
+
+/****************************************************************
+ * DISPLAY SEARCH - where the app will go when a form requests /displaySearch
+ ****************************************************************/
+app.get('/displaySearch', function(req, res) {
+    var type = req.query.type;
+    var start_date = req.query.start_date;
+    var end_date = req.query.end_date;
+    console.log(type, start_date, end_date);
+    if (start_date == '' || end_date == '') {
+        var sql = "SELECT * FROM retreats WHERE type=" + "'" + type + "'";
+    }
+    else {
+        var sql = "SELECT * FROM retreats WHERE type="  + "'" + type + "'" +  "AND start_date >="  + "'" + start_date + "'" +  "AND start_date <="  + "'" + end_date + "'" +  "AND end_date >="  + "'" +  start_date  + "'" + "AND end_date <=" + "'" +  end_date + "'";
+    }
+    //a simple get query from the database
+    pool.query(sql, function(err, result) {
+        // If an error occurred...
+        if (err) {
+            console.log("Error in query: ")
+            console.log(err);
+        }
+
+        var jsonRetreats = JSON.stringify(result.rows);
+        const params = {jsonRetreats: jsonRetreats};
+        displayRetreats(params, res);
+
+
+    }); 
+    // res.send("display all yep");
+
+
+});
+
+
+//listen up
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+
+
+/****************************************************************
+ * DISPLAY RETREATS - callback function that displays retreats nicely
+ ****************************************************************/
+function displayRetreats(params, res) {
+        // Render the response, using the EJS page "result.ejs" in the pages directory
+        // Makes sure to pass it the parameters we need.
+        res.render('./displayAll.ejs', params);
+}
